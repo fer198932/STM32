6610765ebcf2bdf5b4ae2a3b4fca0ac0fd9c7b6b
fer198932 <402154229@qq.com>
2018-12-14 19:24:35 +0800

    ok

--- OIMRobot-400F407-3_20181109_ChangeTools_Young/OIMRobot-400F407-3/USER/ChangeTools.h
+++ OIMRobot-400F407-3_20181109_ChangeTools_Young/OIMRobot-400F407-3/USER/ChangeTools.h
@@ -7,6 +7,12 @@
 
 #define TEST_STEPSNUM 0			// 1: 测试步数； 0: 取消步数测试
 
+#define X_AXIS_RST_CLK 5333			// 归零频率，8细分下5333对应F800
+#define Y_AXIS_RST_CLK 5333			// 归零频率，8细分下5333对应F800
+#define Z_AXIS_RST_CLK 5333			// 归零频率，8细分下5333对应F800
+
+// #define IF_ADDSUB_WORK_CHANGETOOLS					// 控制加减速是否工作的宏，注释后加减速不工作，仅限换刀回零用
+
 void delay_s(u8 delay);
 
 // 保存行走步数的结构体
@@ -16,5 +22,21 @@ typedef struct
 	u32 ySteps[5];
 	u32 zSteps[5];
 } ChangeToolsSavedSteps;
+
+// 脉冲数、频率
+extern u32 xAxisPlusNum,yAxisPlusNum,zAxisPlusNum,aAxisPlusNum,bAxisPlusNum;
+extern u16 xAxisClk,yAxisClk,zAxisClk,aAxisClk,bAxisClk;
+
+extern u8 addSubStepNo;
+extern u32 workedTimeMS;
+extern u8 addcount;
+
+void yAxisUpNStep(u32 steps);
+
+// 分段式加减速-换刀及回零用， nAxis 表示某一轴 0-x, 1-y, 2-z
+void setSpeedRST_Seg(u16 clk, u8 nAxis);
+
+// 连续式加减速-换刀及回零用
+
 
 #endif
--- OIMRobot-400F407-3_20181109_ChangeTools_Young/OIMRobot-400F407-3/USER/ComDataProc.h
+++ OIMRobot-400F407-3_20181109_ChangeTools_Young/OIMRobot-400F407-3/USER/ComDataProc.h
@@ -6,6 +6,8 @@
 
 #define SET_DATA_TEST 1			// 1：开启运动参数的设置，0：关闭
 
+#define IF_ADDSUB_WORK_COMDATAPROC		// 是否进行加减速运动的判断，注释后不加减速
+
 // 保存各个轴设定的运动参数的结构体
 typedef struct 
 {
@@ -17,12 +19,21 @@ typedef struct
 	u16 constTime;					// 匀速运行的时间，是40ms的倍数
 } nAxisSetData;		// 某个轴设定的运动参数
 
+void preProcMaxNum(void);
+u8 preProcStep(void);
+void addSubFlagSet(void);
+void addSubSpeed(void);
+void motor5workstart(void);
+
 extern void selfCheckFunc(void);
 
 // 机械零点复位主程序
 extern void reset2Zero();
 
 extern u8 resetStatusFlag;
+
+// 仅限测试重复定位精度时候用
+void rs232DataProcStep3(void);
 
 #ifdef RESET_DEBUG
 // 从零点到开始位置，debug模式下才开启
