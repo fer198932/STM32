c2d2e5bcbfbc1607a5430e5c2af837ea6098c97d
fer198932 <402154229@qq.com>
2019-02-19 16:59:09 +0800

    offline

---
+++ CoMo-II.20/3.0.0.181224_alpha/OIMRobot-Young-1/DATA/OffLine_Data.h
@@ -0,0 +1,22 @@
+#ifndef _BAJIAOXING_ROUGH_H
+#define _BAJIAOXING_ROUGH_H
+
+#include "sys.h"
+#include "AddSubSpeed_S.h"
+
+#define LENGTH_OFFL 5505
+
+// 暂时三轴
+extern const s32 plusNum_OFFL[LENGTH_OFFL][3];
+
+extern const u32 clk_OFFL[LENGTH_OFFL][3];
+
+
+// 脱机加工
+void offlineWork(u32 num);
+
+// 运动数据设置
+static void offl_Data_Set(u32 num);
+
+#endif
+
