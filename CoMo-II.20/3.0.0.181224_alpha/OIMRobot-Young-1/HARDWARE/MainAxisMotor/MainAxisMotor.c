d25fa7c575b8fbb356b42b7f377bff26990ea91c
fer198932 <402154229@qq.com>
2019-03-06 13:40:56 +0800

    MainMotor

---
+++ CoMo-II.20/3.0.0.181224_alpha/OIMRobot-Young-1/HARDWARE/MainAxisMotor/MainAxisMotor.c
@@ -0,0 +1,254 @@
+#include "StepMotor.h"			
+
+//////////////////////////////////////////////////////////////////////////////////	 
+// 步进电机控制程序
+// 1、
+// 2019年1月31日13:20:00 byYJY
+//////////////////////////////////////////////////////////////////////////////////
+
+// 指令中设定的运动参数
+extern 	volatile 	FunctionalState 		Offline_Work_Flag; 						// 进入脱机加工的标记
+extern 	Flag_Structure 				flag_Struct;
+extern	Motion_Strcuture 			motion_Data;	
+extern	Motion_Strcuture 			motion_Data_Pre;	
+
+
+// 换向间隙相关
+const	 u32	 Backlash_PlusNum = BacklashCompensation * SUBDIV_NUM / (1e3);
+BacklashCompensation_Structure		backlashCompen;
+
+
+// 各轴是否可以运动 
+FunctionalState	 nAxisStatus[AXIS_NUM] = {DISABLE, DISABLE, DISABLE, DISABLE, DISABLE};
+
+// 电机方向IO口
+GPIO_Structure_XX  	StepMotor_Dir[AXIS_NUM];
+
+// 步进电机初始化 主要是方向IO口的初始化
+void StepMotor_Init(void)
+{
+	Motor_Dir_Init(StepMotor_Dir+0, DIR_X);
+	Motor_Dir_Init(StepMotor_Dir+1, DIR_Y);
+	Motor_Dir_Init(StepMotor_Dir+2, DIR_Z);
+	Motor_Dir_Init(StepMotor_Dir+3, DIR_A);
+	Motor_Dir_Init(StepMotor_Dir+4, DIR_B);
+	
+}
+
+// 步进电机运动IO口方向初始化
+static void Motor_Dir_Init(GPIO_Structure_XX *GPIO_Temp, const char str[])
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	
+	// 构造GPIO
+	if(!GPIO_Structure_Make(str, GPIO_Temp)) return;
+	
+	// 使能时钟
+	RCC_AHB1PeriphClockCmd(GPIO_Temp->RCC_Periph_N, ENABLE);
+	
+	// GPIO配置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Temp->GPIO_Pin_N;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;						// 普通输出模式
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;					// 推挽输出
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;			// 100M
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;						// 上拉 默认是正方向运动
+  GPIO_Init(GPIO_Temp->GPIO_Port, &GPIO_InitStructure);									// 初始化GPIO	
+}
+
+// 电机运动方向设定 
+void Motor_Dir_Set(GPIO_Structure_XX *GPIO_Temp, Motor_Dir dir)
+{	
+//	static volatile uint8_t dirOld = 0;  		// 最开始的方向
+	
+	if(TBD_DIR == dir)					// 方向未设定时，啥都不干
+		return;
+	
+	if(POS_DIR == dir) 			// 正向
+	{
+		GPIO_SetBits(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N);
+	}
+	else      							// 负向
+	{
+		GPIO_ResetBits(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N);  
+	}
+	
+//		while(GPIO_ReadOutputDataBit(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N) == dirOld) ; 	// 换向完成退出循环
+//	delay_us(DIR_EX_DALAY); 			// 一定延时
+
+}
+
+// 电机运动方向设定 备份1 
+//void Motor_Dir_Set(GPIO_Structure_XX *GPIO_Temp, Motor_Dir dir)
+//{	
+//	volatile uint8_t dirOld;  		// 最开始的方向
+//	
+//	if(TBD_DIR == dir)					// 方向未设定时，啥都不干
+//		return;
+//	
+//	dirOld = GPIO_ReadOutputDataBit(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N);
+//	
+//	// 需要换向
+//	if(dirOld != dir)
+//	{
+//		if(POS_DIR == dir) 			// 正向
+//		{
+//			GPIO_SetBits(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N);
+//		}
+//		else      							// 负向
+//		{
+//			GPIO_ResetBits(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N);  
+//		}
+//		
+//		while(GPIO_ReadOutputDataBit(GPIO_Temp->GPIO_Port, GPIO_Temp->GPIO_Pin_N) == dirOld) ; 	// 换向完成退出循环
+//		delay_us(DIR_EX_DALAY); 			// 一定延时
+//	}
+//	
+//	// else 不用换向
+//}
+
+// 步进电机同时开始运动（保证同步） 寄存器方式，提高效率
+void StepMotor_Start(void)
+{
+	// 限位触发时，将该轴的脉冲数、频率置零
+	StepMotor_Limited(flag_Struct.Limiti_Flag);
+	
+	// X轴
+	if(ENABLE == nAxisStatus[0])
+	{
+		// 宏定义启动
+		nAxis_StepMotor_Start(X);
+//		PWM_Cmd(X_PWM, ENABLE, X_CH_EXTI);
+//		PWM_Cmd(X_PWM, ENABLE, X_CH_OUT);
+	}
+	
+	// Y轴
+	if(ENABLE == nAxisStatus[1])
+	{
+		nAxis_StepMotor_Start(Y);
+//		PWM_Cmd(Y_PWM, ENABLE, Y_CH_EXTI);
+//		PWM_Cmd(Y_PWM, ENABLE, Y_CH_OUT);	
+	}
+	
+	// Z轴
+	if(ENABLE == nAxisStatus[2])
+	{
+		nAxis_StepMotor_Start(Z);
+//		PWM_Cmd(Z_PWM, ENABLE, Z_CH_EXTI);
+//		PWM_Cmd(Z_PWM, ENABLE, Z_CH_OUT);
+	}
+	
+	// A轴
+	if(ENABLE == nAxisStatus[3])
+	{
+		nAxis_StepMotor_Start(A);
+//		PWM_Cmd(A_PWM, ENABLE, A_CH_EXTI);
+//		PWM_Cmd(A_PWM, ENABLE, A_CH_OUT);
+	}
+	
+	// B轴
+	if(ENABLE == nAxisStatus[4])
+	{
+		nAxis_StepMotor_Start(B);
+//		PWM_Cmd(B_PWM, ENABLE, B_CH_EXTI);
+//		PWM_Cmd(B_PWM, ENABLE, B_CH_OUT);
+	}		
+
+	if(0 == nAxis_Motion_Flag)
+	{
+		flag_Struct.Cmd_Executing_Flag = RESET;
+		
+#if OFFLINE_WORK
+		Offline_Work_Flag = ENABLE;
+#else
+#endif	
+		
+		
+		// 回复上位机
+#if PRIN2DISP
+//		respUsartMsg("PWM_Cmd\r\n", 10);
+#else
+//		respUsartMsg(backResString_Motion, RESP_MOTIONMSG_LENGTH);
+#endif	
+	}
+}
+
+// 步进电机同时停止（同步） 没有强制拉高
+void StepMotor_Stop_Macro(void)
+{
+	TIM_Cmd(X_PWM, DISABLE);
+	TIM_Cmd(Y_PWM, DISABLE);
+	TIM_Cmd(Z_PWM, DISABLE);
+	TIM_Cmd(A_PWM, DISABLE);
+	TIM_Cmd(B_PWM, DISABLE);
+//	nAxis_StepMotor_Stop(X);
+//	nAxis_StepMotor_Stop(Y);
+//	nAxis_StepMotor_Stop(Z);
+//	nAxis_StepMotor_Stop(A);
+//	nAxis_StepMotor_Stop(B);
+}
+
+// 步进电机运行设定距离
+void StepMotor_Move(N_Axis n_axis, u32 Clk, u32 PlusNum, Motor_Dir Dir)
+{		
+	u8 i;
+	// 设定方向
+//	Motor_Dir_Set(StepMotor_Dir+n_axis, Dir);
+
+	/* 各轴初始化 */
+	// 设定运动参数
+	mymemset(&motion_Data_Pre, 0, sizeof(motion_Data_Pre));
+	if(ALL_Axis == n_axis)
+	{
+		for(i=0; i<AXIS_NUM-2; i++)			// 暂时取消A、B轴的运动  byYJY
+		{
+			motion_Data_Pre.cmd_Datas.plus_Datas.plusNum[i] = PlusNum;
+			motion_Data_Pre.cmd_Datas.plus_Datas.clk[i] = Clk;
+			motion_Data_Pre.cmd_Datas.plus_Datas.dir[i] = Dir;
+		}
+	}
+	else
+	{
+		motion_Data_Pre.cmd_Datas.plus_Datas.plusNum[n_axis] = PlusNum;
+		motion_Data_Pre.cmd_Datas.plus_Datas.clk[n_axis] = Clk;
+		motion_Data_Pre.cmd_Datas.plus_Datas.dir[n_axis] = Dir;
+	}
+	
+	// 加减速初始化
+	AddSubSpeedInit_Pre();		// 注意这里可能会对设定的频率进行调整
+	
+	// 运动状态初始化
+	motion_Init();
+	delay_us(MOTION_START_DALAY); 			// 一定延时
+	
+	// 步进电机同时开始运动
+	StepMotor_Start();
+	
+#if NO_ADDSUBSPEED
+	/* 关闭加减速 */
+#else		
+	// 加减速定时器开启
+	EN_ADDSUB_TIMER;
+#endif
+}
+
+// 初始化时候的间隙补偿 0.1mm
+void Move2CompensateBacklash(void)
+{	
+	StepMotor_Move(ALL_Axis, StepMotor_MinClk, SUBDIV_NUM/10, POS_DIR);		// 0.1mm
+	delay_ms(100);
+	StepMotor_Move(ALL_Axis, StepMotor_MinClk, SUBDIV_NUM/10, NEG_DIR);
+	delay_ms(100);
+	
+//	backlashCompen.motorDirOld[i] = NEG_DIR;
+	mymemset((void*)backlashCompen.motorDirOld, NEG_DIR, sizeof(FlagStatus) * AXIS_NUM);
+	
+#if BACKLASH_COMPENSATION
+	backlashCompen.DirChange_En = ENABLE;
+#endif
+}
+
+
+
+
+
+
---
+++ CoMo-II.20/3.0.0.181224_alpha/OIMRobot-Young-1/HARDWARE/MainAxisMotor/MainAxisMotor.h
@@ -0,0 +1,128 @@
+#ifndef __STEPMOTOR_H
+#define	__STEPMOTOR_H	   
+#include "sys.h"
+#include "config.h"
+#include "delay.h"
+#include "pwm.h"
+
+//////////////////////////////////////////////////////////////////////////////////	 
+// 步进电机控制程序
+// 1、
+// 2019年1月31日13:20:00 byYJY
+////////////////////////////////////////////////////////////////////////////////// 	 
+
+#define 	SUBDIV_NUM						400					// 细分数：如400脉冲=1mm
+//#define 	Backlash_PlusNum			BacklashCompensation * SUBDIV_NUM / (1e3)		// 间隙对应的脉冲数
+
+#define		StepMotor_MinClk			500
+#define		StepMotor_MaxClk			10000
+
+
+#define 	DIR_EX_DALAY 					10000 			// 换方向后需要设定一定时间延时  单位us
+#define 	MOTION_START_DALAY 		10000 			// 运动停止后需要设定一定时间延时  单位us
+
+#define 	XAxisDir_Pos 		(Motor_Dir_Set(StepMotor_Dir+0, POS_DIR)) 
+#define 	XAxisDir_Neg 		(Motor_Dir_Set(StepMotor_Dir+0, NEG_DIR)) 
+
+#define 	YAxisDir_Pos 		(Motor_Dir_Set(StepMotor_Dir+1, POS_DIR)) 
+#define 	YAxisDir_Neg 		(Motor_Dir_Set(StepMotor_Dir+1, NEG_DIR)) 
+
+#define 	ZAxisDir_Pos 		(Motor_Dir_Set(StepMotor_Dir+2, POS_DIR)) 
+#define 	ZAxisDir_Neg 		(Motor_Dir_Set(StepMotor_Dir+2, NEG_DIR)) 
+
+#define 	AAxisDir_Pos 		(Motor_Dir_Set(StepMotor_Dir+3, POS_DIR)) 
+#define 	AAxisDir_Neg 		(Motor_Dir_Set(StepMotor_Dir+3, NEG_DIR)) 
+
+#define 	BAxisDir_Pos 		(Motor_Dir_Set(StepMotor_Dir+4, POS_DIR)) 
+#define 	BAxisDir_Neg 		(Motor_Dir_Set(StepMotor_Dir+4, NEG_DIR)) 
+
+// 运动或停止 寄存器方式，提高效率
+// #define 	nAxis_StepMotor_Start(TIM_N) 			((TIM_N)->CR1 |= TIM_CR1_CEN)
+
+#define 	nAxis_Motion_Flag 	(nAxisStatus[0] | nAxisStatus[1] | nAxisStatus[2] | nAxisStatus[3] | nAxisStatus[4])
+
+
+// 换向，间隙补偿的结构体
+typedef struct {
+	volatile		Motor_Dir 					motorDirOld[AXIS_NUM];					// 前一次的运动方向
+	volatile		FlagStatus					DirChange_Flag[AXIS_NUM];				// 换向的标志
+	volatile		FunctionalState			DirChange_En;										// 初始状态关闭	
+} BacklashCompensation_Structure;
+
+
+#define	nAxis_StepMotor_Start(n) 	\
+do \
+{	\
+	PWM_Cmd(n##_PWM, ENABLE, n##_CH_EXTI); 	\
+	PWM_Cmd(n##_PWM, ENABLE, n##_CH_OUT); 	\
+} while(0)
+
+// #define 	nAxis_StepMotor_Stop(TIM_N) 			((TIM_N)->CR1 &= (uint16_t)~TIM_CR1_CEN)
+#define	nAxis_StepMotor_Stop(n) 	\
+do \
+{	\
+	PWM_Cmd(n##_PWM, DISABLE, n##_CH_EXTI); 	\
+	PWM_Cmd(n##_PWM, DISABLE, n##_CH_OUT); 	\
+} while(0)
+
+extern 	GPIO_Structure_XX  	StepMotor_Dir[AXIS_NUM];
+
+// typedef enum {TBD_DIR = 0, POS_DIR = 1, NEG_DIR = 2} Motor_Dir; 		// 电机运动方向
+
+// 步进电机初始化 主要是方向IO口的初始化
+void StepMotor_Init(void);
+
+// 步进电机运动IO口方向初始化
+static void Motor_Dir_Init(GPIO_Structure_XX *GPIO_Temp, const char str[]);
+
+// 电机运动方向设定
+void Motor_Dir_Set(GPIO_Structure_XX *GPIO_Temp, Motor_Dir dir);
+
+// 步进电机同时开始运动（保证同步） 寄存器方式，提高效率
+void StepMotor_Start(void);
+
+// 步进电机同时停止（同步） 没有强制拉高
+void StepMotor_Stop_Macro(void);
+
+
+// 步进电机运行设定距离
+void StepMotor_Move(N_Axis n_axis, u32 Clk, u32 PlusNum, Motor_Dir Dir);
+
+// 初始化时候的间隙补偿
+void Move2CompensateBacklash(void);
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
